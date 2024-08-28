17e934b6c7317bcd623b556efd243434a8c11d59
Abhijith12371 <abpokeunite@gmail.com>
2024-08-28 06:10:07 +0530

    secondCommit

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const PassportLocalMongoose=require("passport-local-mongoose")
-mongoose.connect('mongodb://127.0.0.1:27017/logintest')
-  .then(() => console.log('Connected!'));
 
+
+mongoose.connect('mongodb://127.0.0.1:27017/logintest', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 let userSchema=new mongoose.Schema({
     email:{
